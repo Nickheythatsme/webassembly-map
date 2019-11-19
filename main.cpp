4b35febc5d5d21829df7b2d7a1120d47b8127e1d
Nicholas Grout <njgrout@gmail.com>
2019-11-18 21:37:00 -0800

    modified:   CMakeLists.txt 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <emscripten/emscripten.h>
-#include <emscripten/html5.h>
-#include "src/shapefile.h"
 #include <sstream>
 #include <iostream>
 
+#if  __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#include <emscripten/html5.h>
+#endif
+
+#include "src/shapefile.h"
+
 int main()
 {
-  std::stringstream ss;
-  uint16_t value = 0x0123;
-  uint16_t network_value = htons(value);
-  ss.write((char*)&network_value, sizeof(network_value));
-  uint16_t response = readShortBE(ss);
-  std::cout << value << std::endl;
-  std::cout << response << std::endl;
-  return 0;
+    uint64_t val = 0xffaaff;
+    uint64_t val2 = 0;
+    reverse_value(&val, &val2);
+    std::stringstream ss;
+    uint16_t value = 0x0123;
+    uint16_t network_value = htons(value);
+    ss.write((char*)&network_value, sizeof(network_value));
+    uint16_t response = readShortBE(ss);
+    std::cout << value << std::endl
+            << response << std::endl;
+    return 0;
 }
