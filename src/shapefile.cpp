1eed481fb06db7d4e3a2317a7b7f9afe6620f810
nick-grout <nick@plusqa.com>
2019-11-18 16:59:30 -0800

    modified:   main.cpp 	new file:   opengl_example.cpp 	modified:   src/shapefile.cpp 	modified:   src/shapefile.h

--- src/shapefile.cpp
+++ src/shapefile.cpp
@@ -1,5 +1,22 @@
 #include "shapefile.h"
 
-void test() {
-    htons(1);
+
+/**
+Parse out a big endian short
+*/
+uint16_t parse_be_short(std::istream& in)
+{
+    uint16_t val = 0;
+    in.read(reinterpret_cast<char*>(&val), sizeof(val));
+    return ntohs(val);
+}
+
+/**
+Parse out a big endian long
+*/
+uint32_t parse_be_long(std::istream& in)
+{
+    uint32_t val = 0;
+    in.read(reinterpret_cast<char*>(&val), sizeof(val));
+    return ntohl(val);
 }
--- src/shapefile.h
+++ src/shapefile.h
@@ -1,3 +1,12 @@
 #include <netinet/in.h>
+#include <istream>
 
-void test();
+/**
+Parse out a big endian short
+*/
+uint16_t parse_be_short(std::istream&);
+
+/**
+Parse out a big endian long
+*/
+uint32_t parse_be_long(std::istream&);
